d42d748efba569afba88516162fd03e545b823db
tommy1713 <ryota.sg@gmail.com>
2020-01-29 21:12:16 +0900

    product_detail2

---
+++ db/migrate/20200129102525_create_products.rb
@@ -0,0 +1,19 @@
+class CreateProducts < ActiveRecord::Migration[5.2]
+  def change
+    create_table :products do |t|
+      t.references :user
+      t.string :name, null: false
+      t.text :detail, null: false
+      t.integer :category, null: false
+      t.string :brand
+      t.string :size
+      t.integer :price, null: false
+      t.integer :status, null: false
+      t.integer :shipping_area, null: false
+      t.integer :estimated_date, null: false
+      t.integer :postage, null: false
+      t.integer :situation, null: false
+      t.timestamps
+    end
+  end
+end
