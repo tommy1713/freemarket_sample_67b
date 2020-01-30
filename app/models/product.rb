84e59d3d30cecdd32a75260ae2498f16af24d00d
tommy1713 <ryota.sg@gmail.com>
2020-01-30 12:54:24 +0900

    product_detail4

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,3 +1,5 @@
 class Product < ApplicationRecord
-  
+  enum status: [:unused, :near_unused, :no_dirt, :dirt_small, :dirt, :bad]
+  enum postage: [:buyer, :vendor]
+  enum estimated_date: [:early, :half, :late]
 end
