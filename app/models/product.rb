f1f2fe81bf42015693f7a389a59bf8430b8e2a80
tommy1713 <ryota.sg@gmail.com>
2020-01-31 18:00:55 +0900

    ex2

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,5 +2,5 @@ class Product < ApplicationRecord
   enum status: [:unused, :near_unused, :no_dirt, :dirt_small, :dirt, :bad]
   enum postage: [:buyer, :vendor]
   enum estimated_date: [:early, :half, :late]
-  enum area: []
+  enum shipping_area: [:hokkaido, :aomori, :iwate, :miyagi, :akita, :yamagata, :fukushima, :ibaraki, :tochigi, :gunnma, :saitama, :chiba, :tokyo, :kanagawa, :niigata, :toyama, :ishikawa, :fukui, :yamanashi, :nagano, :gihu, :shizuoka, :aichi, :mie, :shiga, :kyoto, :osaka, :hyogo, :nara, :wakayama, :tottori, :shimane, :okayama, :hiroshima, :yamaguchi, :tokushima, :kagawa, :ehime, :kouchi, :fukuoka, :saga, :nagasaki, :kumamoto, :ooita, :miyazaki, :kagoshima, :okinawa]
 end
