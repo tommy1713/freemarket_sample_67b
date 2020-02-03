6dc630c59719de1d468f33932f8e11671e059754
nagamachan <59175719+nagamachan@users.noreply.github.com>
2020-02-03 20:52:51 +0900

    logoutpage.setting

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,12 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "homes#index"
-  resources :users, only:[:index, :show, :new]
+  resources :users, only:[:index, :show, :new, :post]
   resources :products, only:[:show]
+
+  resources "users",only: :logout, path: '' do
+    collection do
+      get 'logout'
+    end
+  end
 end
