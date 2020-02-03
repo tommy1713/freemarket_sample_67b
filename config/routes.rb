0780c1ff44294824f7f0dab71fa7deec29668e59
nagamachan <59175719+nagamachan@users.noreply.github.com>
2020-02-03 21:24:06 +0900

    routes.rb update

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "homes#index"
-  resources :users, only:[:index, :show, :new, :post]
+  resources :users, only:[:index, :show, :new, :detroy]
   resources :products, only:[:show]
 
   resources "users",only: :logout, path: '' do
