caaebe7970e8fa566813ad26209eff965f3f2f8d
tommy1713 <ryota.sg@gmail.com>
2020-02-04 15:47:21 +0900

    ex4

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: "homes#index"
-  resources :products, only:[:show]
+  resources :products, only:[:show, :new]
 end
