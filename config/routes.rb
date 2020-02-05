f330829421d7c7cade568eae6be21c8324764fbe
u-suke922 <eyrui.ctakemuri@icloud.com>
2020-02-06 01:32:58 +0900

    step1.view

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,13 @@
 Rails.application.routes.draw do
   devise_for :users
-  root to: "homes#index"
+  root to: "sign_up#step1"
   resources :users, only:[:index, :show, :new]
   resources :products, only:[:show]
   resources :users, only: :show
   resources :confirmations, only: :index
+  resources :sign_up do
+    member do
+      get 'information'
+    end
+  end
 end
