b0374ae28252b1f35ee813a8df9b5b81c2facfa9
nagamachan <59175719+nagamachan@users.noreply.github.com>
2020-02-05 20:06:09 +0900

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -11,11 +11,10 @@ Rails.application.routes.draw do
     end
   end
 
-  resources :card, only: [:index, :new, :show] do
+  resources :card, only: [:index, :new, :show, :destroy] do
     collection do
       post 'show', to: 'card#show'
       post 'pay', to: 'card#pay'
-      post 'delete', to: 'card#destoroy'
     end
   end
 end
