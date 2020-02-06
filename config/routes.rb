7a753d203675f18e47ae82a545355e2ecc2f35e1
u-suke922 <eyrui.ctakemuri@icloud.com>
2020-02-06 21:20:00 +0900

    routes.修正

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
-  root to: "sign_up#information-first"
+  root to: "homes#index"
   resources :users, only:[:index, :show, :new, :destroy]
   resources :products, only:[:show, :new]
   resources :confirmations, only: :index
