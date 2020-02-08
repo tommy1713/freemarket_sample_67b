d0edf49203f8ece048b835ca17c4b7e7903fc3c8
yutaro60 <yasaka0407@gmail.com>
2020-02-08 16:33:30 +0900

    deleate2

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,4 +1,4 @@
-class ProductsController < ApplicationController
+class ProductsController < ApplicationController, with: :render_404
 
   def show 
     @product = Product.find(params[:id])
@@ -12,5 +12,17 @@ class ProductsController < ApplicationController
     product = Product.find(params[:id])
     product.destroy
     @users = User.all
+    def render_404(e = nil)
+      if e
+        logger.error e 
+        logger.error e.backtrace.join("\n") 
+      end
+      render template: 'errors/error404', status: 404, layout: 'application', content_type: 'text/html'
+    end
+  end
+
+  private
+  def set_product
+    @product = Product.find(params[:id])
   end
 end
