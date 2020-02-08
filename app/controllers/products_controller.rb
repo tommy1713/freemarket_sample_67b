da053d0f9b7dc74b3031e2d8bb158527c9a27273
yutaro60 <yasaka0407@gmail.com>
2020-02-08 16:08:38 +0900

    deleate

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -2,8 +2,15 @@ class ProductsController < ApplicationController
 
   def show 
     @product = Product.find(params[:id])
+    @parents = Category.where(ancestry: nil)
   end
 
   def new
+  end
+
+  def destroy
+    product = Product.find(params[:id])
+    product.destroy
+    @users = User.all
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_02_05_081055) do
+ActiveRecord::Schema.define(version: 2020_02_05_080908) do
 
   create_table "cards", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.integer "user_id", null: false
@@ -33,20 +33,6 @@ ActiveRecord::Schema.define(version: 2020_02_05_081055) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["product_id"], name: "index_images_on_product_id"
-  end
-
-  create_table "items", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.string "name", null: false
-    t.text "state", null: false
-    t.string "condition", null: false
-    t.integer "price", null: false
-    t.integer "level", default: 0
-    t.integer "likes_count", default: 0
-    t.integer "root_category_id"
-    t.integer "buyer_id"
-    t.integer "seller_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
   end
 
   create_table "products", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
@@ -73,20 +59,14 @@ ActiveRecord::Schema.define(version: 2020_02_05_081055) do
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.string "confirmation_token"
-    t.datetime "confirmed_at"
-    t.datetime "confirmation_sent_at"
-    t.string "unconfirmed_email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "username"
-    t.string "encrypted_password"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
-    t.index ["username"], name: "index_users_on_username", unique: true
   end
 
 end
