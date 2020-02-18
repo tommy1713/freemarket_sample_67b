36c8c51b6f2527c44b8b09a11127800e3c7659fe
yutaro60 <yasaka0407@gmail.com>
2020-02-18 14:03:32 +0900

    comment

--- app/assets/javascripts/dialog.js
+++ app/assets/javascripts/dialog.js
@@ -1,12 +1,10 @@
 $(document).on('turbolinks:load', function() {
   $("#destroy--btn").click(function(e){
-    console.log("open")
     e.preventDefault();
     $('#dialog').fadeIn();
   });
 
   $('#close').click(function() {
-    console.log("close")
     $('#dialog').fadeOut();
   });
 });
--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,15 +1,16 @@
 class CommentsController < ApplicationController
 
   def create
-    comment = Comment.create(comment_params)
-    # respond_to do |format|
-    #   format.html{redirect_to product_path(params[:product_id])}
-    #   format.json
-    redirect_to product_path(params[:product_id])
+    @comment = Comment.create(comment_params)
+    respond_to do |format|
+      format.html{redirect_to product_path(comment_params[:product_id])}
+      format.json
+    end
   end
 
   private
   def comment_params
     params.require(:comment).permit(:text).merge(user_id: current_user.id, product_id: params[:product_id])
   end
+
 end
